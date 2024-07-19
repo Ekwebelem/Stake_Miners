1463c75c8b1dfacc46ee870757a00a682f049c0a
eBarry <64811352+Ekwebelem@users.noreply.github.com>
2024-07-19 12:39:06 +0100

    Update urls.py

--- stake_miners/urls.py
+++ stake_miners/urls.py
@@ -20,7 +20,5 @@ from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
